9515ddd59c105b23566de2710d1bb0e2b1e345ca
Vince Duarte <vinceduarte3@gmail.com>
2021-10-06 14:38:21 -0400

    game is now linear array - also updated dataset to local dir - board is no longer 3x3 matrix

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -7,13 +7,12 @@ def print_board(grid):
     board = []
 
     for x in grid:
-        for y in x:
-            if (y == 1):
-                board.append('X')
-            elif (y == -1):
-                board.append('O')
-            else:
-                board.append(' ')
+        if (x == 1):
+            board.append('X')
+        elif (x == -1):
+            board.append('O')
+        else:
+            board.append(' ')
 
     print("\n")
     print("\t     |     |")
@@ -30,55 +29,75 @@ def print_board(grid):
     print("\t     |     |")
     print("\n")
 
-def check_pos(grid, x, y):
-    if (grid[x][y] == 0):
+def check_pos(grid, pos):
+    if (grid[pos] == 0):
         return True
     else:
         return False
 
-def check_win(board, x, y):
+def check_win(board):
     # win logic credited to https://codereview.stackexchange.com/questions/24764/tic-tac-toe-victory-check
     # returns true if columns or diagonals in matrix are same value
-    if board[0][y] == board[1][y] == board [2][y]:
+    if board[0] == board[3] == board[6] != 0:
         return True
-    if board[x][0] == board[x][1] == board [x][2]:
+    if board[1] == board[4] == board[7] != 0:
         return True
-    if x == y and board[0][0] == board[1][1] == board [2][2]:
+    if board[2] == board[5] == board[8] != 0:
         return True
-    if x + y == 2 and board[0][2] == board[1][1] == board [2][0]:
+    if board[0] == board[1] == board[2] != 0:
         return True
+    if board[3] == board[4] == board[5] != 0:
+        return True    
+    if board[6] == board[7] == board[8] != 0:
+        return True 
+    if board[0] == board[4] == board[8] != 0:
+        return True 
+    if board[2] == board[4] == board[6] != 0:
+        return True 
 
     return False          
 
 def check_draw(board):
     # returns true only if all spots on the board are full
     for x in board:
-        for y in x:
-            if (y == 0):
-                return False
+        if (x == 0):
+            return False
 
     return True
 
 def user_input(grid):
     valid = False
     while valid is False:
-        pos_x = int(input("Enter a row: "))
-        if (pos_x in range(0,3)):
-            pos_y = int(input("Enter a col: "))
-            if (pos_y in range(0,3)):
-                if check_pos(grid, pos_x, pos_y):
+        pos = int(input("Enter a position: "))
+        if (pos in range(0,9)):
+                if check_pos(grid, pos):
                     valid = True
-                    return pos_x, pos_y
+                    return pos
                 else:
                     print("Not allowed.")
-            else:
-                print("Enter a valid position.")
         else:
-            print("Enter a valid position.")
+            print("Not a valid position.")
 
 def main():
     # numpy matrix for Tic-Tac-Toe board
-    grid = np.zeros(shape=(3,3), dtype=int)
+    grid = np.zeros(shape=(9), dtype=int)
+
+    # print reference for positions
+    print("\n")
+    print("Position input:")
+    print("\t     |     |")
+    #print("\t  {}  |  {}  |  {}".format(grid[0,0], grid[0,1], grid[0,2]))
+    print("\t  {}  |  {}  |  {}".format("0", "1", "2"))
+    print('\t_____|_____|_____')
+    print("\t     |     |")
+    #print("\t  {}  |  {}  |  {}".format(grid[1,0], grid[1,1], grid[1,2]))
+    print("\t  {}  |  {}  |  {}".format("3", "4", "5"))
+    print('\t_____|_____|_____')
+    print("\t     |     |")
+    #print("\t  {}  |  {}  |  {}".format(grid[2,0], grid[2,1], grid[2,2]))
+    print("\t  {}  |  {}  |  {}".format("6", "7", "8"))
+    print("\t     |     |")
+    print("\n")
     
     # game logic loops while True
     game_state = True
@@ -86,10 +105,13 @@ def main():
         print_board(grid)
 
         print("Player 1 turn.")
-        x, y = user_input(grid)
-        grid[x][y] = 1
+        pos = user_input(grid)
+        grid[pos] = 1
+
         print_board(grid)
-        if check_win(grid, x, y):
+        
+        # check if previous move was a win or draw
+        if check_win(grid):
             print("Player 1 wins!")
             game_state = False
             break
@@ -100,10 +122,13 @@ def main():
                 break   
 
         print("Player 2 turn.")
-        x, y = user_input(grid)
-        grid[x][y] = -1
+        pos = user_input(grid)
+        grid[pos] = -1
+
         print_board(grid)
-        if check_win(grid, x, y):
+        
+        # check if previous move was a win or draw
+        if check_win(grid):
             print("Player 2 wins!")
             game_state = False
             break
