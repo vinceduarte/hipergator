fee541dc50e7be2d137e0267fb8c88cdecf33786
nick2 <nsalazar1@ufl.edu>
2021-10-28 21:58:30 -0400

    Added data augmentation

--- Part_2/ImageNeuralNetwork.py
+++ Part_2/ImageNeuralNetwork.py
@@ -3,7 +3,7 @@ import os
 import glob
 import numpy as np
 import tensorflow as tf
-import torch as t
+import torch as tr
 import torch.nn as nn
 import torch.nn.functional as Fn
 import math
@@ -16,9 +16,14 @@ import random as rd
 img_dir = "face_images/*.jpg"
 files = glob.glob(img_dir)
 data = []
+i = 0
 for f in files:
     img = cv2.imread(f)
     data.append(img)
+
+data = np.asarray(data)
+data = tf.convert_to_tensor(data, dtype=tf.float32)
+print("Loaded")
 
 
 # data = tf.convert_to_tensor(data)
@@ -37,12 +42,7 @@ for f in files:
 # Scale Image BGR by between 0.6 and 1.0
 def aug_scale_rgb(image):
     random_scale = rd.uniform(0.6, 1.0)
-    for p_i in range(len(image[0])):
-        for p_j in range(len(image)):
-            for c in range(3):
-                # Scale the BGR by a random amount
-                image[p_i][p_j][c] = int(image[p_i][p_j][c] * random_scale)
-    return image
+    return image * random_scale
 
 
 # Crop Image and resize
@@ -64,27 +64,48 @@ def aug_flip(image):
     return cv2.flip(image, 1)
 
 
+def perform_cb_aug(image):
+    flip = rd.randint(1, 3)
+    crop = rd.randint(1, 3)
+    bgr = rd.randint(1, 3)
+    if flip == 1:
+        image = aug_flip(image)
+    if crop == 1:
+        image = aug_crop(image)
+    if bgr == 1:
+        image = aug_scale_rgb(image)
+    return image
+
+
 scale = 10
 # aug_data = tf.fill([len(data) * scale, 128, 128, 3], 0)
 curr = 0
+aug_data = np.array(data)  # .expand(scale*data.size(), 128, 128, 3)
 # For each image in data
-for img in data:
-    # Do this n times for each image
-    cv2.imshow("original", img)
-    cv2.waitKey(0)
-    resized = aug_crop(img)
-    flipped = aug_flip(img)
-    bgr = aug_scale_rgb(img)
-    # for i in range(1, scale):
-    # curr += 1
+print("Augment Data")
+for img in data.numpy():
+    print(curr)
+    curr += 1
+    aug_data = np.append(aug_data, aug_flip(img))
+    aug_data = np.append(aug_data, aug_crop(img))
+    aug_data = np.append(aug_data, aug_scale_rgb(img))
+    aug_data = np.append(aug_data, aug_crop(aug_flip(img)))
+    aug_data = np.append(aug_data, aug_scale_rgb(aug_flip(img)))
+    aug_data = np.append(aug_data, aug_scale_rgb(aug_crop(img)))
+    aug_data = np.append(aug_data, aug_scale_rgb(aug_crop(img)))
+    aug_data = np.append(aug_data, aug_scale_rgb(aug_crop(aug_flip(img))))
+    aug_data = np.append(aug_data, aug_scale_rgb(aug_crop(aug_flip(img))))
+
+aug_data = tf.convert_to_tensor(aug_data)
+
+idx = tr.randperm(aug_data.shape[0])
+aug_data = aug_data[idx].view(aug_data.size())
+# Do this n times for each image
+# for i in range(1, scale):
+# curr += 1
 
 # Convert data to L*a*b* color space (Luminance+Chrominance Space)
-dataLAB = []
-i = 0
-# TODO: change "data" to "aug_data
-for img in data:
-    imgLAB = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    dataLAB.append(imgLAB)
+dataLAB = cv2.cvtColor(aug_data, cv2.COLOR_BGR2LAB)
 
 print(dataLAB[0])
 
@@ -133,6 +154,7 @@ class Net(nn.Module):
         x = Fn.relu(x)
         x = self.conv7(x)
         x = Fn.relu(x)
+        return x
 
 # Forward
 # Compute Loss
